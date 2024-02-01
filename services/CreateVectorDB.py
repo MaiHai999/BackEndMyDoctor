15ab09607b575f1f0ed66b89b923b9705105076f
MaiHai999 <maihai.tt@gmail.com>
2024-02-01 23:51:12 +0700

    okla

--- services/CreateVectorDB.py
+++ services/CreateVectorDB.py
@@ -2,35 +2,39 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import GPT4AllEmbeddings
+import os
+import glob
 
 
 #khai báo và khởi tạo một số biến
-pdf_data_path_vn = "../../asset/dataset/Vietnamese-languageBooks"
-pdf_data_path_en = "../../asset/dataset/English-languageBooks"
-vector_db_path_vn = "../../asset/database/db_vector_vn"
-vector_db_path_en = "../../asset/database/db_vector_en"
+pdf_data_path_vn = "../../dataset/Vietnamese-languageBooks"
+pdf_data_path_en = "../../dataset/English-languageBooks"
+vector_db_path_vn = "../../database/db_vector_vn"
+vector_db_path_en = "../../database/db_vector_en"
 
 
-# Khai bao loader de quet toan bo thu muc data
-loader = DirectoryLoader(pdf_data_path_vn, glob="*.pdf", loader_cls=PyPDFLoader)
-documents = loader.load()
-
-#Cắt nhỏ văn bản
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=50)
-chunks = text_splitter.split_documents(documents)
-
-# Embeding
+list_file_paths = glob.glob(os.path.join(pdf_data_path_vn, '*.pdf'))
 embedding_model = GPT4AllEmbeddings()
-db = FAISS.from_documents(chunks, embedding_model)
-db.save_local(vector_db_path_vn)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=50)
 
+new_db = FAISS.load_local(vector_db_path_vn, embedding_model)
 
+for path in list_file_paths:
+    try:
+        print(path)
+        loader = PyPDFLoader(path)
+        documents = loader.load()
 
+        # Cắt nhỏ văn bản
+        chunks = text_splitter.split_documents(documents)
 
+        # Embeding
+        db = FAISS.from_documents(chunks, embedding_model)
 
+        #Meger
+        new_db.merge_from(db)
 
+    except Exception as e:
+        continue
 
-
-
-
-
+new_db.save_local(vector_db_path_vn)
