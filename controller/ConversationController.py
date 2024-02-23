76a5005ed44603d631afffb5d36415ea654fd7ff
MaiHai999 <maihai.tt@gmail.com>
2024-02-23 22:59:40 +0700

    hi

--- controller/ConversationController.py
+++ controller/ConversationController.py
@@ -8,7 +8,6 @@ from flask_jwt_extended import create_refresh_token
 from flask_jwt_extended import get_jwt_identity, get_jwt
 from flask_jwt_extended import unset_jwt_cookies
 from flask_jwt_extended import jwt_required
-from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.exc import NoResultFound
 
 from flask import Blueprint
@@ -26,26 +25,38 @@ db_manager.connect()
 session = db_manager.get_session()
 
 
-@con_blueprint.route('/get_conversation', methods=["GET"])
+@con_blueprint.route('/get_all', methods=["GET"])
 @jwt_required()
-def get_conversation():
+def get_all_conversation():
     try:
         identity = get_jwt_identity()
-        id_user = identity['id_user']
-        user = EntityHandler.get_entity_id(session , User , id_user)
-        conversation_list = []
-        for conversation in user.conversations:
-            conversation_dict = {
-                'id': conversation.id,
-                'title': conversation.title,
-                'create_date': conversation.create_date.strftime('%Y-%m-%d %H:%M:%S'),
-                'id_user': conversation.id_user,
-                'status': conversation.status
-            }
-            conversation_list.append(conversation_dict)
+        role = identity['role']
+        if role == 2:
+            id_user = identity['id_user']
+            user = EntityHandler.get_entity_id(session , User , id_user)
+            conversation_list = []
+            for conversation in user.conversations:
+                conversation_dict = {
+                    'id': conversation.id,
+                    'title': conversation.title,
+                    'create_date': conversation.create_date.strftime('%Y-%m-%d %H:%M:%S'),
+                    'id_user': conversation.id_user,
+                    'status': conversation.status
+                }
+                conversation_list.append(conversation_dict)
 
-        return jsonify(conversation_list), 200
+            return jsonify(conversation_list), 200
+
+        else:
+            response = jsonify({"mes": "Vai trò admin"})
+            return response, 200
+
+
     except Exception as e:
         error_message = "Error: {}".format(str(e))
         response = jsonify({"error": error_message})
         return response, 500
+
+
+
+
