4e95fc7bfb0bb7d42cb3b1f21eb9b8f072264476
MaiHai999 <maihai.tt@gmail.com>
2024-03-25 14:13:04 +0700

    okla

--- Config.py
+++ Config.py
@@ -22,7 +22,7 @@ os.environ["tk_user"] = 'root'
 os.environ["password_tk_user"] = '01692032691'
 
 os.environ["SECRET_KEY_JWT"] = 'haideptrai'
-os.environ["base_url"] = "http://192.168.1.2:9999/v1"
+os.environ["base_url"] = "http://192.168.1.5:9999/v1"
 
 os.environ["path_vector_db"] = project_root + "/BackEnd/asset/database/db_vector_en"
 
